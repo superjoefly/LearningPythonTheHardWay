0a0a7986096ebaf81be0bb8775254c65acf30fe3
superjoefly <superjoefly@gmail.com>
2017-12-23 20:42:44 -0700

    Initial Commit

---
+++ ex1.py
@@ -0,0 +1,75 @@
+# print 'Time to count...'
+# print 'Colors: ', 25 + 30/6
+# print 'Day: ', 100 - 25 * 3 % 4
+# print 5 < 3
+# print 5 <= 5
+# print "What is 5 - 7", 5 - 7
+# print 'Hello there, today is December ' + "23"
+# print 5/2
+# print 5%2
+# print 7.0 / 4.0
+# print 7 / 4
+# print round(1.544);
+
+
+
+
+# Lesson 4: Variables and Names
+# cars = 100
+# space_in_car = 4.0
+# drivers = 30
+# passengers = 90
+# cars_not_driven = cars - drivers
+# cars_driven = drivers
+# carpool_capacity = cars_driven * space_in_car
+# average_passengers_in_car = passengers / cars_driven
+#
+# print "There are", cars, "cars available."
+# print "There are only", drivers, "drivers available."
+# print "We can transport", carpool_capacity, "people today."
+# print "We have", passengers, "to carpool today."
+# print "We need to put about", average_passengers_in_car, "in each car."
+
+
+
+
+# Lesson 5: Format Characters
+# name = "Joey A"
+# age = 38
+# height = 75
+# weight = 175 # I think...
+# eyes = 'blue'
+# teeth = 'white'
+# hair = 'brown'
+#
+# # Overwrite variable
+# hair = 'green'
+#
+# print "Let's talk about %s." % name;
+# print "He's %d inches tall." % height;
+# print "He's %d pounds heavy." % weight;
+# print "Actually, that's not too heavy!";
+# print "He's got %s eyes and %s hair." % (eyes, hair);
+# print "His teeth are usually %s depending on the coffee." % teeth;
+#
+# # Adding Variables
+# print "If I add %d, %d, and %d I get %d." % (age, height, weight, age + height + weight);
+
+# # A couple more format characters
+# # %r Print no matter what
+#
+# print "I overwrote the variable for hair with %r." % hair;
+#
+# # Convert inches to centimeters
+# number_of_inches = 5;
+# inches_to_centimeters = 2.54;
+#
+# # Write string with embedded variables
+# print "%s inches in centimeters is %s times %s" % (number_of_inches, inches_to_centimeters, number_of_inches)
+#
+# # Write string and perform computation after string using variables
+# print "The result of the above computation is", number_of_inches * inches_to_centimeters;
+
+
+
+# Lesson 6:
